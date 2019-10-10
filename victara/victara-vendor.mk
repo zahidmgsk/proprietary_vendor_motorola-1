bcaaa0fe74f4a590ce516237e70a7cdbe57cafae
jro1979oliver <jroliveira.oliveira301@gmail.com>
2019-10-10 13:14:05 +0000

    victara: add libsdm-disp-apis.so for use with LiveDisplay

--- victara/victara-vendor.mk
+++ victara/victara-vendor.mk
@@ -284,4 +284,5 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/victara/proprietary/vendor/lib/mediadrm/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so \
     vendor/motorola/victara/proprietary/vendor/lib/soundfx/libqcbassboost.so:system/vendor/lib/soundfx/libqcbassboost.so \
     vendor/motorola/victara/proprietary/vendor/lib/soundfx/libqcreverb.so:system/vendor/lib/soundfx/libqcreverb.so \
-    vendor/motorola/victara/proprietary/vendor/lib/soundfx/libqcvirt.so:system/vendor/lib/soundfx/libqcvirt.so
+    vendor/motorola/victara/proprietary/vendor/lib/soundfx/libqcvirt.so:system/vendor/lib/soundfx/libqcvirt.so\
+    vendor/motorola/victara/proprietary/lib/libsdm-disp-apis.so:system/lib/libsdm-disp-apis.so
